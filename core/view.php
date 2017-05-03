16a50e7a96099415e6f22b508b09a1367bc64e00
Sonja <sonja@sonjas-mbp.infocom.local>
2017-05-03 16:25:01 +0200

    some fixes and improvements

--- core/view.php
+++ core/view.php
@@ -4,34 +4,34 @@
     <div class="sidebar-content section">
       <h2 class="hgroup hgroup-single-line hgroup-compressed cf">
         <span class="hgroup-title">
-          <a href="#">Blueprints</a>
+          <a href="">Blueprints</a>
         </span>
       </h2>
       <ul class="nav nav-list sidebar-list">
         <?php foreach($blueprints as $blueprint): ?>
-          <li><a class="load-file" href="#" data-target="blueprints/<?= $blueprint ?>"><i class="icon icon-left fa fa-file-o"></i><span><?=  $blueprint ?></span></a></li>
+          <li><a class="load-file" href="" data-target="blueprints/<?= $blueprint ?>"><i class="icon icon-left fa fa-file-o"></i><span><?=  $blueprint ?></span></a></li>
         <?php endforeach ?>
       </ul>
 
       <h2 class="hgroup hgroup-single-line hgroup-compressed cf">
        <span class="hgroup-title">
-         <a href="#">Templates</a>
+         <a href="">Templates</a>
        </span>
       </h2>
       <ul class="nav nav-list sidebar-list">
         <?php foreach($templates as $template): ?>
-          <li><a class="load-file" href="#" data-target="templates/<?= $template ?>"><i class="icon icon-left fa fa-file-o"></i><span><?=  $template ?></span></a></li>
+          <li><a class="load-file" href="" data-target="templates/<?= $template ?>"><i class="icon icon-left fa fa-file-o"></i><span><?=  $template ?></span></a></li>
         <?php endforeach ?>
       </ul>
 
       <h2 class="hgroup hgroup-single-line hgroup-compressed cf">
         <span class="hgroup-title">
-          <a href="#">Snippets</a>
+          <a href="">Snippets</a>
         </span>
       </h2>
       <ul class="nav nav-list sidebar-list">
         <?php foreach($snippets as $snippet): ?>
-          <li><a class="load-file" href="#" data-target="snippets/<?= $snippet ?>"><i class="icon icon-left fa fa-file-o"></i><span><?=  $snippet ?></span></a></li>
+          <li><a class="load-file" href="" data-target="snippets/<?= $snippet ?>"><i class="icon icon-left fa fa-file-o"></i><span><?=  $snippet ?></span></a></li>
         <?php endforeach ?>
       </ul>
     </div>
@@ -39,17 +39,12 @@
 
   <div class="mainbar">
     <div class="section">
-      <h2 class="hgroup cf">
-        <span>
-           Editor
-          <span class="counter"></span>
-        </span>
-      </h2>
+      <h2 class="hgroup cf"><span>Editor</span></h2>
 
       <form method="post" action="" class="form" data-keep="">
         <fieldset class="fieldset field-grid cf">
           <div class="field field-grid-item field-name-text field-with-textarea field-with-buttons">
-            <label class="label" for="form-field-text">Text</label>
+            <label class="label label-filename" for="form-field-text"></label>
             <div class="field-content">
           </div>
           </div>
@@ -61,6 +56,8 @@
   </div>
 
 </div>
+<?= css(url() . '/site/plugins/code-editor/assets/css/styles.css') ?>
+
 <?= js(url() . '/site/plugins/code-editor/assets/js/codemirror.js') ?>
 <?= css(url() . '/site/plugins/code-editor/assets/css/codemirror.css') ?>
 <?= js(url() . '/site/plugins/code-editor/assets/js/xml.js') ?>
@@ -72,6 +69,12 @@
 <?= js(url() . '/site/plugins/code-editor/assets/js/yaml.js') ?>
 <?= js(url() . '/site/plugins/code-editor/assets/js/php.js') ?>
 <script>
+//$(document).ready(function(){
+  $('h2').click(function() {
+    $(this).next('ul').slideToggle();
+  });
+//});
+
   var myCodeMirror = CodeMirror(function(elt) {
     $('.field-content').append(elt);
   },
@@ -82,10 +85,11 @@
     }
   );
 
-  $('.load-file').on('click', function(e) {
+  $('.load-file').on('click', function() {
     console.log('click');
+    $('.label-filename').html('');
     $('.load-file.active').removeClass('active');
-    e.preventDefault();
+    //e.preventDefault();
     $(this).addClass('active');
     var url = $(this).data('target');
     console.log(url);
@@ -109,7 +113,9 @@
       type: 'GET',
 
       success: function(response) {
+        console.log(response);
         myCodeMirror.setValue(response);
+        $('.label-filename').append(url);
       }
     });
   };
